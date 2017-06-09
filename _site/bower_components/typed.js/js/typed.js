d42a93e4fc0118429612bb0720177d1534fbbf4b
harish gandhi ramachandran <hramachandran.haw.iit.edu>
2017-06-09 11:11:55 -0500

    roles

--- _site/bower_components/typed.js/js/typed.js
+++ _site/bower_components/typed.js/js/typed.js
@@ -456,7 +456,7 @@
 	window.Typed = Typed;
 
 	var defaults = {
-		strings: ["These are the default values...", "You know what you should do?", "Use your own!", "Have a great day!"],
+		strings: ["Data analyst", "Software Developer", "Entrepreneur", "Foodie", "Sports Fanatic","Book Reader"],
 		stringsElement: null,
 		// typing speed
 		typeSpeed: 0,
